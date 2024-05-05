210ab768cbf18a45d02905a6e224b60ffe3dac9b
Daniil Kashapov <kdv200265@gmail.com>
2024-05-05 15:35:29 +0500

    task 3

--- solution.py
+++ solution.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_1samp
 
+chat_id = 441218136 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+def solution(x: np.array) -> bool:
+    # Perform the one-sample t-test
+    t_stat, p_value = ttest_1samp(x, 500)
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    # Return True if we reject the null hypothesis (p-value < 0.06), False otherwise
+    return p_value < 0.06
